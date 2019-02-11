1f1b33d81d3c7c90ab9c1423081d28f629f03e60
IanMcG32 <46964673+IanMcG32@users.noreply.github.com>
2019-02-11 23:26:47 +0000

    Update models.py

--- app/profile/models.py
+++ app/profile/models.py
@@ -30,7 +30,7 @@ class Education(db.Model):
     location = db.Column(db.String, nullable=True)
     degree_award_year = db.Column(db.String, nullable=True)
     
-class Team(db.Model):
+class TeamMembers(db.Model):
 
     __tablename__ = 'team_members'
 
@@ -44,3 +44,22 @@ class Team(db.Model):
     def __init__(self, start_date, end_date, name, position, grant_number):
         self.start_date = start_date
         self.end_date = end_date
+        self.name = name
+        self.position = position
+        self.grant_number = grant_number
+
+class Teams(db.Model):
+
+    __tablename__ = 'teams'
+
+    researcher = db.Column(db.String(20),nullable=False)
+    grant_number = db.Column(db.Integer,nullable=False)
+    team = db.Column(db.TeamMembers, nullable=False)
+    id = db.Column(db.Integer, primary_key=True) #Primary Key needed
+
+    def __init__(self, researcher, grant_number, team):
+        self.reseacher = researcher
+        self.team = team
+        self.grant_number = grant_number
+
+
